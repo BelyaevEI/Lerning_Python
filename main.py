d792410ca2e36a412dc2a9f9ed04ab4773289974
BelyaevEI <50379634+BelyaevEI@users.noreply.github.com>
2022-06-25 11:09:48 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import Model
-import View
-import Control
-
-model = Model.MinesweeperModel()
-controller = Control.MinesweeperController(model)
-view = View.MinesweeperView(model, controller)
-view.pack()
-view.mainloop()
